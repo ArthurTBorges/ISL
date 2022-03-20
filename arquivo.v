f02817ed310172e1b4c088b4e5bb94c5127f9070
ArthurTBorges <94976005+ArthurTBorges@users.noreply.github.com>
2022-03-20 12:11:30 -0300

    Update arquivo.v

--- arquivo.v
+++ arquivo.v
@@ -1,112 +1,107 @@
-module maquina (clk, res, entrada, saida, ctrl); 
-   
-  input [6:0] entrada; 
-  input clk, res, ctrl; 
-  output [3:0] saida; 
-  reg [3:0] estado; //Registrar os estados 
+module maquina(clk, res, entrada, saida, ax);
+    
+ input wire clk, res;
+ input wire [7:0]entrada;
+ output reg [3:0]saida;
+ output ax;
+ reg res2;
+ reg[3:0]state;
+
+ parameter  
+     inic = 4'b0000, //cod dos estados 
+     est1 = 4'b0001, 
+     est2 = 4'b0010, 
+     est3 = 4'b0011, 
+     est4 = 4'b0100, 
+     est5 = 4'b0101, 
+     fals = 4'b1000, 
+     acao = 4'b1001, 
+     hora = 4'b1010;
  
-  parameter 
-      inic = 4'b0000, //cod dos estados 
-      est1 = 4'b0001, 
-      est2 = 4'b0010, 
-      est3 = 4'b0011, 
-      est4 = 4'b0100, 
-      est5 = 4'b0101, 
-      fals = 4'b1000, 
-      acao = 4'b1001, 
-      hora = 4'b1010; 
- 
-  initial begin 
-     estado <= inic; 
-  end 
- 
-  always @(negedge res, negedge clk) begin 
-     if(res == 1'b1) estado <= inic; 
-     else begin 
-         case(estado) 
-             inic: begin //Estado inicial 
-                 if(ctrl == 1'b1) 
-                     case(entrada) 
-                         7'b0010000: estado <= est1; 
-                         7'b0100100: estado <= est2; 
-                         7'b0000010: estado <= est3; 
-                         7'b1000111: estado <= est4; 
-                         7'b0111010: estado <= est5; 
-                         default: estado <= inic; 
-                      endcase 
-                 else estado <= inic; 
-             end 
- 
-             est1: begin //Estado 1 
-                 if(ctrl == 1) 
-                     case(entrada) 
-                         7'b0100100: estado <= est2; 
-                         7'b0101001: estado <= fals; //c7 
-                         default: estado <= acao; //c6 
-                     endcase 
-                 else estado <= est1; 
-             end 
- 
-             est2: begin //Estado 2 
-                 if(ctrl == 1) 
-                     case(entrada) 
-                         7'b0010000: estado <= est1; 
-                         7'b0000010: estado <= est3; 
-                         7'b0101001: estado <= fals; 
-                         default: estado <= acao; 
-                     endcase 
-                 else estado <= est2; 
-             end 
- 
-             est3: begin //Estado 3 
-                 if(ctrl == 1) 
-                     case(entrada) 
-                         7'b0100100: estado <= est2; 
-                         7'b1000111: estado <= est4; 
-                         7'b0101001: estado <= fals; 
-                         default: estado <= acao; 
-                     endcase 
-                 else estado <= est3; 
-             end 
- 
-             est4: begin //Estado 4 
-                 if(ctrl == 1) 
-                     case(entrada) 
-                         7'b0000010: estado <= est3; 
-                         7'b0111010: estado <= est5; 
-                         7'b0101001: estado <= fals; 
-                         default: estado <= hora; //c8 
-                     endcase 
-                 else estado <= est4; 
-             end 
- 
-             est5: begin //Estado 5 
-                 if(ctrl == 1) 
-                     case(entrada) 
-                         7'b1000111: estado <= est4; 
-                         7'b0101001: estado <= fals; 
-                         default: estado <= hora; //c8 
-                     endcase 
-                 else estado <= est5; 
-             end 
- 
-             acao: begin // ação/c6 
-                 estado <= acao; 
-             end 
- 
-             fals: begin // falso/c7 
-                 estado <= fals; 
-             end 
- 
-             hora: begin // hora/c8 
-                 estado <= hora; 
-             end 
-         endcase 
-     end 
-  end 
- 
-  always @(estado) begin 
-     case(estado) 
+ parameter
+     c1: 8'b10010000,
+     c2: 8'b10100100,
+     c3: 8'b10000010,
+     c4: 8'b11000111,
+     c5: 8'b10111010,
+     c6: 8'b10011110, //ação
+     c7: 8'b10101001, //flaso
+     c8: 8'b10001101; //hora
+
+ initial begin
+     state <= inic;
+ end
+
+ always @ (posedge clk, posedge res) begin
+     res2 = res;
+     if(res2 = 1'b1) begin
+         state <= inic;
+         res2 <= 1'b0;
+     end
+     else begin
+         case(state)
+             inic: begin
+                 case (entrada)
+                      c1: state <= est1;
+                      c2: state <= est2;
+                      c3: state <= est3;
+                      c4: state <= est4;
+                      c5: state <= est5;
+                 endcase
+             end
+
+             est1: begin
+                 case(entrada)
+                     c1: state <= est1;
+                     c2: state <= est2;
+                     c6: state <= acao;
+                     default: <= fals;
+                 endcase
+             end
+
+             est2: begin
+                 case(entrada)
+                     c1: state <= est1;
+                     c2: state <= est2;
+                     c3: state <= est3;
+                     c6: state <= acao;
+                     default: <= fals;
+                 endcase
+             end
+
+             est3: begin
+                 case(entrada)
+                     c2: state <= est2;
+                     c3: state <= est3;
+                     c4: state <= est4;
+                     c6: state <= acao;
+                     default: <= fals;
+                 endcase 
+             end
+
+             est4: begin
+                 case(entrada)
+                     c3: state <= est3;
+                     c4: state <= est4;
+                     c5: state <= est5;
+                     c8: state <= hora;
+                     default: <= fals;
+                 endcase
+             end
+
+             est5: begin
+                 case(entrada)
+                     c4: state <= est4;
+                     c5: state <= est5;
+                     c8: state <= hora;
+                     default: <= fals;
+                 endcase
+         endcase
+     end
+ end
+
+ always @ (state) begin
+     case(state)
          inic: saida = 4'b0000; 
          est1: saida = 4'b0001; 
          est2: saida = 4'b0010; 
@@ -115,8 +110,10 @@ module maquina (clk, res, entrada, saida, ctrl);
          est5: saida = 4'b0101; 
          acao: saida = 4'b1001; 
          fals: saida = 4'b1000; 
-         hora: saida = 4'b1010; 
-     endcase 
-  end     
- 
+         hora: saida = 4'b1010;
+     endcase
+ end
+
+ assign ax = res2;
+
 endmodule
